bd0de7a525999b31dfc5018661206e7f2410a1f7
Hack Reactor Students <accounts@hackreactor.com>
2017-01-19 17:34:15 -0800

    Refactor post-solution video

--- app/config-mongo.js
+++ app/config-mongo.js
@@ -1,25 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost/shortlyTest');
+mongoose.connect('mongodb://localhost:27017/shortly-deploy');
 
-//setting up schema in mongoose
-var usersSchema = new Schema({
-  id: Number,
-  username: String,
-  password: String,
-  createdAt: String,
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function() {
+  console.log('Mongodb connection succeeded');
 });
 
-var urlSchema = new Schema({
-  id: Number,
-  url: String,
-  baseUrl: String,
-  code: String,
-  title: String,
-  visits: Number,
-  createdAt: String
-});
-
-module.exports.user = mongoose.model('User', userSchema);
-module.exports.url = mongoose.model('Url', urlSchema);
+module.exports = db;
--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,33 @@
-var db = require('../config-mongo');
 var bcrypt = require('bcrypt-nodejs');
+var mongoose = require('mongoose');
 var Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function() {
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function() {
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+var userSchema = mongoose.Schema({
+  username: { type: String, index: { unique: true } },
+  password: String
 });
+
+var User = mongoose.model('User', userSchema);
+
+User.prototype.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, thispassword, function(err, isMatch) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, isMatch);
+    }
+  });
+};
+
+userSchema.pre('save', function(next) {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      thispassword = hash;
+      next();
+    });
+});
+
+var pete = new User({username: 'pete'}).save();
 
 module.exports = User;
